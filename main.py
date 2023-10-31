1445b7eb3ce6fb4fffa63df9a15007775a23b4e8
hamzamasoodUF <hamzamasood@ufl.edu>
2023-10-31 16:13:57 -0400

    Hamza's edited part

--- main.py
+++ main.py
@@ -15,6 +15,14 @@ def console():
     print("2. Decode")
     print("3. Quit")
 
+def decoder(encoded_pass)
+    array_input = [int(char) for char in password]
+    for i in range(len(array_input):
+    new = array_input[i] - 3
+    array_input[i] = (new_digit + 10) % 10
+  original = ''.join(map(str, array_input))
+
+  return original
 
 running = 1
 while running == 1:
@@ -31,8 +39,10 @@ while running == 1:
             encoded_pass = encode(user_pass)
             print("Your password has been encoded and stored!")
         if menu == 2:
-            print("n/a")
+            decode = decoder(encoded_pass)
+            print(f"the encoded password is {encoded_pass}, the original password is {user_pass}")
         if menu == 3:
             break
+
 
 
